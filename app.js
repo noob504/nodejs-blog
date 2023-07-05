77aa7459a9c53e699c0b9e478071b30113148dc2
noob504 <luciefer21062002@gmail.com>
2023-07-05 13:49:11 +0530

    completed homepage and liked with mongodb

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const connectDB = require('./server/config/db.js');
+
+// initializing express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connecting to database
+connectDB();
+
+// middleware & template engine
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+// serving static files
+app.use(express.static("public"));
+
+// routing
+app.use("/", require("./server/routes/main"));
+
+// server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
